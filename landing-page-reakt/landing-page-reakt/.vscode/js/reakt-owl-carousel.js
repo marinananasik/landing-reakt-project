eccf05ec44692d0804f6d0efc52209f8e5dd9825
Marina <kulinicmarina91@gmail.com>
2022-12-25 20:08:27 +0300

    first commit

---
+++ landing-page-reakt/landing-page-reakt/.vscode/js/reakt-owl-carousel.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+
+<OwlCarousel className='owl-theme' loop margin={50} nav>
+    <div class='conteiner'>
+        <div class='row align-items-center'>
+            <div class="col-lg-5 mr-auto aos-init aos-animate" data-aos="fade-up">
+                        <div class="owl-carousel slide-one-item-alt-text owl-loaded owl-drag">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage"
+                                    style="transform: translate3d(-890px, 0px, 0px); transition: all 0s ease 0s; width: 3560px;">
+                                    <div class="owl-item cloned" style="width: 445px;">
+                                        <div class="slide-text">
+                                            <h1>High Quality Website Templates</h1>
+                                            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
+                                            <p><a href="#" target="_blank" class="btn btn-outline-light py-3 px-5">Get
+                                                    Started</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="owl-item cloned" style="width: 445px;">
+                                        <div class="slide-text">
+                                            <h1>We do things right.</h1>
+                                            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
+                                            <p><a href="#" target="_blank" class="btn btn-outline-light py-3 px-5">Get
+                                                    Started</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="owl-item active" style="width: 445px;">
+                                        <div class="slide-text">
+                                            <h1>We Do Awesome Things</h1>
+                                            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
+                                            <p><a href="#" target="_blank" class="btn btn-outline-light py-3 px-5">Get
+                                                    Started</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="owl-item" style="width: 445px;">
+                                        <div class="slide-text">
+                                            <h1>Welcome</h1>
+                                            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
+                                            <p><a href="#" target="_blank" class="btn btn-outline-light py-3 px-5">Get
+                                                    Started</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="owl-item" style="width: 445px;">
+                                        <div class="slide-text">
+                                            <h1>High Quality Website Templates</h1>
+                                            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
+                                            <p><a href="#" target="_blank" class="btn btn-outline-light py-3 px-5">Get
+                                                    Started</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="owl-item" style="width: 445px;">
+                                        <div class="slide-text">
+                                            <h1>We do things right.</h1>
+                                            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
+                                            <p><a href="#" target="_blank" class="btn btn-outline-light py-3 px-5">Get
+                                                    Started</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="owl-item cloned" style="width: 445px;">
+                                        <div class="slide-text">
+                                            <h1>We Do Awesome Things</h1>
+                                            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
+                                            <p><a href="#" target="_blank" class="btn btn-outline-light py-3 px-5">Get
+                                                    Started</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="owl-item cloned" style="width: 445px;">
+                                        <div class="slide-text">
+                                            <h1>Welcome</h1>
+                                            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                                Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
+                                            <p><a href="#" target="_blank" class="btn btn-outline-light py-3 px-5">Get
+                                                    Started</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="owl-nav disabled">
+                                <div class="owl-prev">prev</div>
+                                <div class="owl-next">next</div>
+                            </div>
+                            <div class="owl-dots">
+                                <div class="owl-dot active"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                                <div class="owl-dot"><span></span></div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    <div class='item'>
+        <h4>2</h4>
+    </div>
+    <div class='item'>
+        <h4>3</h4>
+    </div>
+  
+</OwlCarousel>;
